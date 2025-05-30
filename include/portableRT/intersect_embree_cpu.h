525a8b918e1078cdc71a47e99ad3b1376c412a79
101001000 <kike.delacalle.montilla@gmail.com>
2025-05-30 16:26:57 +0200

    embree single tris improvement

--- include/portableRT/intersect_embree_cpu.h
+++ include/portableRT/intersect_embree_cpu.h
@@ -34,77 +34,7 @@ RTCDevice initializeDevice()
   return device;
 }
 
-/*
- * Create a scene, which is a collection of geometry objects. Scenes are 
- * what the intersect / occluded functions work on. You can think of a 
- * scene as an acceleration structure, e.g. a bounding-volume hierarchy.
- *
- * Scenes, like devices, are reference-counted.
- */
-RTCScene initializeScene(RTCDevice device, const std::array<float, 9>& vs)
-{
-  RTCScene scene = rtcNewScene(device);
 
-  /* 
-   * Create a triangle mesh geometry, and initialize a single triangle.
-   * You can look up geometry types in the API documentation to
-   * find out which type expects which buffers.
-   *
-   * We create buffers directly on the device, but you can also use
-   * shared buffers. For shared buffers, special care must be taken
-   * to ensure proper alignment and padding. This is described in
-   * more detail in the API documentation.
-   */
-  RTCGeometry geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
-  float* vertices = (float*) rtcSetNewGeometryBuffer(geom,
-                                                     RTC_BUFFER_TYPE_VERTEX,
-                                                     0,
-                                                     RTC_FORMAT_FLOAT3,
-                                                     3*sizeof(float),
-                                                     3);
-
-  unsigned* indices = (unsigned*) rtcSetNewGeometryBuffer(geom,
-                                                          RTC_BUFFER_TYPE_INDEX,
-                                                          0,
-                                                          RTC_FORMAT_UINT3,
-                                                          3*sizeof(unsigned),
-                                                          1);
-
-  if (vertices && indices)
-  {
-    vertices[0] = vs[0]; vertices[1] = vs[1]; vertices[2] = vs[2];
-    vertices[3] = vs[3]; vertices[4] = vs[4]; vertices[5] = vs[5];
-    vertices[6] = vs[6]; vertices[7] = vs[7]; vertices[8] = vs[8];
-
-    indices[0] = 0; indices[1] = 1; indices[2] = 2;
-  }
-
-  /*
-   * You must commit geometry objects when you are done setting them up,
-   * or you will not get any intersections.
-   */
-  rtcCommitGeometry(geom);
-
-  /*
-   * In rtcAttachGeometry(...), the scene takes ownership of the geom
-   * by increasing its reference count. This means that we don't have
-   * to hold on to the geom handle, and may release it. The geom object
-   * will be released automatically when the scene is destroyed.
-   *
-   * rtcAttachGeometry() returns a geometry ID. We could use this to
-   * identify intersected objects later on.
-   */
-  rtcAttachGeometry(scene, geom);
-  rtcReleaseGeometry(geom);
-
-  /*
-   * Like geometry objects, scenes must be committed. This lets
-   * Embree know that it may start building an acceleration structure.
-   */
-  rtcCommitScene(scene);
-
-  return scene;
-}
 
 /*
  * Cast a single ray with origin (ox, oy, oz) and direction
@@ -150,16 +80,76 @@ public:
         static RegisterBackend reg(*this);
     }
 
+    void initializeScene()
+    {
+
+      rtcSetSceneFlags(m_scene, RTC_SCENE_FLAG_DYNAMIC);
+      m_tri = rtcNewGeometry(m_device, RTC_GEOMETRY_TYPE_TRIANGLE);
+
+      float* vertices = (float*) rtcSetNewGeometryBuffer(m_tri,
+                                                        RTC_BUFFER_TYPE_VERTEX,
+                                                        0,
+                                                        RTC_FORMAT_FLOAT3,
+                                                        3*sizeof(float),
+                                                        3);
+
+      unsigned* indices = (unsigned*) rtcSetNewGeometryBuffer(m_tri,
+                                                              RTC_BUFFER_TYPE_INDEX,
+                                                              0,
+                                                              RTC_FORMAT_UINT3,
+                                                              3*sizeof(unsigned),
+                                                              1);
+
+      if (vertices && indices)
+      {
+        vertices[0] = 0; vertices[1] = 0; vertices[2] = 0;
+        vertices[3] = 0; vertices[4] = 0; vertices[5] = 0;
+        vertices[6] = 0; vertices[7] = 0; vertices[8] = 0;
+
+        indices[0] = 0; indices[1] = 1; indices[2] = 2;
+      }
+
+      /*
+      * You must commit geometry objects when you are done setting them up,
+      * or you will not get any intersections.
+      */
+      rtcCommitGeometry(m_tri);
+
+      /*
+      * In rtcAttachGeometry(...), the scene takes ownership of the geom
+      * by increasing its reference count. This means that we don't have
+      * to hold on to the geom handle, and may release it. The geom object
+      * will be released automatically when the scene is destroyed.
+      *
+      * rtcAttachGeometry() returns a geometry ID. We could use this to
+      * identify intersected objects later on.
+      */
+      rtcAttachGeometry(m_scene, m_tri);
+
+      /*
+      * Like geometry objects, scenes must be committed. This lets
+      * Embree know that it may start building an acceleration structure.
+      */
+      rtcCommitScene(m_scene);
+    }
+
     bool intersect_tri(const std::array<float,9>& tri, const Ray& ray) const {
       
-          RTCScene scene = initializeScene(m_device, tri);
+          float* vertices = (float*) rtcGetGeometryBufferData(m_tri, RTC_BUFFER_TYPE_VERTEX, 0);
+          for (int i = 0; i < 9; i++) {
+              vertices[i] = tri[i];
+          }
+          rtcCommitGeometry(m_tri);
+          rtcCommitScene(m_scene);
+
+
 
           /* This will hit the triangle at t=1. */
-          bool res = castRay(scene, ray.origin[0], ray.origin[1], ray.origin[2], ray.direction[0], ray.direction[1], ray.direction[2]);
+          bool res = castRay(m_scene, ray.origin[0], ray.origin[1], ray.origin[2], ray.direction[0], ray.direction[1], ray.direction[2]);
 
           /* Though not strictly necessary in this example, you should
           * always make sure to release resources allocated through Embree. */
-          rtcReleaseScene(scene);
+
           return res;
     }
 
@@ -179,14 +169,20 @@ public:
 
     void init() override {
       m_device = initializeDevice();
+      m_scene = rtcNewScene(m_device);
+      initializeScene();
     }
 
     void shutdown() override{
+      rtcReleaseGeometry(m_tri);
+      rtcReleaseScene(m_scene);
       rtcReleaseDevice(m_device);
     }
 
 private:
   RTCDevice m_device;
+  RTCScene m_scene;
+  RTCGeometry m_tri;
 };
 
 
